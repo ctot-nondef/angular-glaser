e50d4405cf77cc3cbf0f77b866d5f7fab2a7fedd
Christoph <christoph.hoffmann@oeaw.ac.at>
2016-08-18 09:39:46 +0200

    partials and css, forgot to include in last commit

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -38,49 +38,13 @@ GlaserControllers
     //};
 }])
 .controller('GlaserNav', function ($scope, $timeout, $mdSidenav, $log) {
-    $scope.toggleLeft = buildDelayedToggler();
-    /**
-     * Supplies a function that will continue to operate until the
-     * time is up.
-     */
-    function debounce(func, wait, context) {
-      var timer;
-      return function debounced() {
-        var context = $scope,
-            args = Array.prototype.slice.call(arguments);
-        $timeout.cancel(timer);
-        timer = $timeout(function() {
-          timer = undefined;
-          func.apply(context, args);
-        }, wait || 10);
-      };
-    }
-    /**
-     * Build handler to open/close a SideNav; when animation finishes
-     * report completion in console
-     */
-    function buildDelayedToggler() {
-      return debounce(function() {
+    $scope.toggleLeft = function () {
         // Component lookup should always be available since we are not using `ng-if`
-        $mdSidenav('left')
-          .toggle()
+        $mdSidenav('left').toggle()
           .then(function () {
             $log.debug("toggle is done");
           });
-      }, 200);
-    }
-    function buildToggler() {
-      return function() {
-        // Component lookup should always be available since we are not using `ng-if`
-        $mdSidenav('left')
-          .toggle()
-          .then(function () {
-            $log.debug("toggle is done");
-          });
-      }
-    }
-  })
-  .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    };
     $scope.close = function () {
       // Component lookup should always be available since we are not using `ng-if`
       $mdSidenav('left').close()
@@ -88,4 +52,4 @@ GlaserControllers
           $log.debug("close LEFT is done");
         });
     };
-  });
+  })
