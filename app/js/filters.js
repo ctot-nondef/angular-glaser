06cfee6e90fa55c14d6e23ddbb0405c4aee3dea5
Christoph Hoffmann <Christoph.Hoffmann@oeaw.ac.at>
2016-09-15 18:10:49 +0200

    minor bugfixes / layout corrections

--- app/js/filters.js
+++ app/js/filters.js
@@ -2,7 +2,23 @@
 
 /* Filters */
 
-angular.module('GlaserFilters', []).filter('checkmark', function() {
+angular.module('GlaserFilters', [])
+.filter('checkmark', function() {
+  return function(input) {
+    return input ? '\u2713' : '\u2718';
+  };
+})
+.filter('hasTranslation', function() {
+  return function(input) {
+    return input ? '\u2713' : '\u2718';
+  };
+})
+.filter('hasTranscription', function() {
+  return function(input) {
+    return input ? '\u2713' : '\u2718';
+  };
+})
+.filter('geoGeonamesID', function() {
   return function(input) {
     return input ? '\u2713' : '\u2718';
   };
