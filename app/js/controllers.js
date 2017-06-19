abf60cbf996d7f0e2ef86660889cb4d87b456306
Christoph Hoffmann <Christoph.Hoffmann@oeaw.ac.at>
2017-06-19 12:23:08 +0200

    quick access from results page

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -79,7 +79,6 @@ GlaserApp
         opacsearch.updateSize(b);
         $scope.promise = opacsearch.getRecordsbyIndex('collect.inf', opacsearch.history.query[$stateParams.queryID-1],"AND",undefined,[],$stateParams.pageNo);
         opacsearch.updatePage($stateParams.queryID-1, $stateParams.pageNo, $scope.promise);
-        console.log($scope.promise);
         $scope.promise.then($scope.update);
       }
       else $state.go('gl.results', {queryID: $stateParams.queryID, pageNo: a});
@@ -129,7 +128,7 @@ GlaserApp
     $scope.promise = opacsearch.history.result[$stateParams.queryID-1][$stateParams.pageNo];
   }
   else {
-    $scope.promise = opacsearch.getRecordsbyIndex('collect.inf', opacsearch.history.query[$stateParams.queryID-1],"AND",undefined,['priref','production.place','production.place.lref','production.place.context','production.place.uri','inscription.language', 'title', 'reproduction.reference'],$stateParams.pageNo);
+    $scope.promise = opacsearch.getRecordsbyIndex('collect.inf', opacsearch.history.query[$stateParams.queryID-1],"AND",undefined,['priref','production.place','production.place.lref','production.place.context','production.place.uri','inscription.language', 'title', 'reproduction.reference','object_number'],$stateParams.pageNo);
     opacsearch.updatePage($stateParams.queryID-1, $stateParams.pageNo, $scope.promise);
   }
   $scope.promise.then($scope.update);
