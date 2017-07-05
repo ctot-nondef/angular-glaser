65f7c549a9d2a30c9f29d1d0f087e17114d6c185
Christoph Hoffmann <Christoph.Hoffmann@oeaw.ac.at>
2017-07-05 15:16:39 +0200

    .

--- app/js/ngTEI.js
+++ app/js/ngTEI.js
@@ -41,7 +41,7 @@ ngTEI.service('TEI', function($http, $localStorage, $q, $log){
     doc = doc.replace(/<([a-zA-Z0-9 ]+)(?:xml)ns=\".*\"(.*)>/g, "<$1$2>");
     var xml = oParser.parseFromString(doc, "text/xml").querySelector("TEI");
     for (var s in this.CONFIG.TEIConfigObject) {
-      let nl = xml.querySelectorAll(String(s));
+      let nl = xml.querySelectorAll(String(s).split('_')[0]);
       let tr = this.CONFIG.TEIConfigObject[s];
       let els = [];
       for(var i = nl.length; i--; els.unshift(nl[i]));
@@ -60,23 +60,11 @@ ngTEI.service('TEI', function($http, $localStorage, $q, $log){
             els[idx].setAttribute(a, tr.setAttribute[a]);
           }
         }
-        //WRAP IN A SPECIFIED ELEMENT
-        if(tr.wrapElement){
-          let newElement = document.createElementNS('http://www.w3.org/1999/xhtml',tr.wrapElement.tag);
-          for (var a in tr.wrapElement.attributes) {
-            newElement.setAttribute(a, tr.wrapElement.attributes[a]);
-            newElement.setAttribute('id', 'wrapper');
-          }
-          newElement.innerHTML = els[idx].outerHTML;
-          els[idx].replaceWith(newElement);
-          els[idx] = xml.querySelector('[id=wrapper]').firstElementChild;
-          els[idx].removeAttribute('id');
-        }
         //WRAP CONTENT IN A SPECIFIED ELEMENT
         if(tr.wrapContent){
-          let newElement = document.createElementNS('http://www.w3.org/1999/xhtml',tr.wrapElement.tag);
-          for (var a in tr.wrapElement.attributes) {
-            newElement.setAttribute(a, tr.wrapElement.attributes[a]);
+          let newElement = document.createElementNS('http://www.w3.org/1999/xhtml',tr.wrapContent.tag);
+          for (var a in tr.wrapContent.attributes) {
+            newElement.setAttribute(a, tr.wrapContent.attributes[a]);
           }
           newElement.innerHTML = els[idx].innerHTML;
           els[idx].innerHTML = newElement.outerHTML;
@@ -84,12 +72,28 @@ ngTEI.service('TEI', function($http, $localStorage, $q, $log){
         //INSERTS A DIV WITH SPECIFIED MARKUP AT SPECIFIED POSITION
         //SEE LIST OF AVAILABLE POSITIONS AT https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentElement
         if(tr.insertElement){
+          console.log('insert', els[idx]);
           let el = document.createElement('div');
           el.innerHTML = tr.insertElement.markup;
           els[idx].insertAdjacentElement(tr.insertElement.position, el);
         }
+        //THESE MUST BE DONE LAST, AS REPLACEMENT OF THE ELEMENT IN QUESTION RENDERS THE LOOP UNUSABLE
+        //WRAP IN A SPECIFIED ELEMENT
+        if(tr.wrapElement){
+          console.log("wrap", els[idx]);
+          let newElement = document.createElementNS('http://www.w3.org/1999/xhtml',tr.wrapElement.tag);
+          for (var a in tr.wrapElement.attributes) {
+            newElement.setAttribute(a, tr.wrapElement.attributes[a]);
+            newElement.setAttribute('wrapper', '');
+          }
+          newElement.innerHTML = els[idx].outerHTML;
+          console.log('newElement', newElement);
+          els[idx].replaceWith(newElement);
+          els[idx] = xml.querySelector('[wrapper]').firstElementChild;
+          els[idx].removeAttribute('wrapper');
+          console.log("wrap", xml.querySelectorAll('[wrapper]'));
+        }
         //REPLACE DEFINED ELEMENTS
-        //THIS MUST BE DONE LAST, AS IT RENDERS THE LOOP VARS UNUSABLE
         if(tr.replaceElement){
           let newElement = document.createElementNS('http://www.w3.org/1999/xhtml',tr.replaceElement);
           newElement.innerHTML = els[idx].innerHTML;
@@ -128,24 +132,3 @@ ngTEI.directive('teidoc', ['$compile', '$http', '$q', 'TEI', function ($compile,
         link: link
     };
 }]);
-
-ngTEI.directive('teiheader', function () {
-    return {
-      transclude: true,
-      template: '<div class="md-whiteframe-3dp" layout-padding layout-margin ng-transclude></div>'
-    };
-});
-
-ngTEI.directive('facsimile', function () {
-    return {
-      transclude: true,
-      template: '<div class="md-whiteframe-3dp" layout-padding layout-margin ng-transclude></div>'
-    };
-});
-
-ngTEI.directive('title', function () {
-    return {
-      transclude: true,
-      template: '<h3><ng-transclude></ng-transclude></h3>'
-    };
-});
